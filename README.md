02b06dd7802b1799857bf5f9a942958a5e49e12d
Flamerinus <alexmosuyevon@yahoo.es>
2018-01-19 05:49:56 +0100

    animated everything coded all the logic made small/mobile friendly

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
-#Tres en raya
+# Tres en raya
+
 [Click here to see it in action.](http://flamerinus.byethost7.com/3enraya/)
 ___
-This is the popular Tic-Tac-Toe (X & O) game, created with the use of the CSS grid and JavaScript.
+This is the popular Tic-Tac-Toe (X & O) game, created with the use of the CSS grid and JavaScript. [Animate.css](https://cdnjs.com/libraries/animate.css/) was used to animate the tiles and text.
 (Work in progress)
 
 Made while learning **JavaScript** and the **CSS grid**.
 ___
 The important files are:
-+ **index2.html**: Main html file containing all the elements: buttons, description. etc
++ **index.html**: Main html file containing all the elements: buttons, description. etc
 + **sketch2.js**: JavaScript sketch called in the .html file, contains the code, functions and variables.
 + **lang.js**: Second JavaScript sketch called in the index. Contains the translations and language changing functions.(NOT READY YET)
 ___
--- sketch2.js
+++ sketch2.js
@@ -1,6 +1,10 @@
 "use strict"
 var turno= 1;
 var element;
+var a = 1;
+var t;
+var done;
+var tremble;
 //funcion constructora de celdas
 function Cell(empty,value){
 	this.e=empty;
@@ -10,31 +14,48 @@ var juego=[];//array vacio donde meto las 9 celdas
 for(var i=0;i<9;i++){
 	juego.push(new Cell(true,"n"));
 }
+
+
+
 /*funcion que pone los valores de vacio a	verdadero en todas las celdas, los
 colores, turnos etc, */
 function resetear() {
-		juego=[];
-		for(i=0;i<9;i++){
+			done= false;
+	 		t = document.getElementById("texto");
+			juego=[];
+			for(var x=0;x<9;x++){
+			let elemid = document.getElementById("cell"+x);
 			juego.push(new Cell(true,"n"));
-			document.getElementById("cell"+i).innerHTML = "";
+			elemid.innerHTML = "";
+			let cl = elemid.className;
+			elemid.className="";
+			setTimeout(function(){elemid.className=cl;},100);
 					}
+					turno = 1;
+					tremble.innerHTML= deftext;
+					tremble.className="";
+					t.style.backgroundColor="#e34a06";
 }
 function clickerino(n){
-	if(juego[n].e==true){
+	tremble = document.getElementById("trem");
+	t = document.getElementById("texto");
+
+	element = document.getElementById("cell"+n);
+	if(juego[n].e==true && !done){
 		switch(turno){
 			case 1:
 				juego[n].e=false;
 				juego[n].v="x";
-				element = document.getElementById("cell"+n);
 				element.style.color = "#e34a06";
 				element.innerHTML = "X";
+				t.style.backgroundColor="#407026";
 				turno = 2;
 				break;
 			case 2:
 				juego[n].e=false;
 				juego[n].v="o";
-				element = document.getElementById("cell"+n);
 				element.style.color = "#407026";
+				t.style.backgroundColor="#e34a06";
 				element.innerHTML = "O";
 				turno = 1;
 				break;
@@ -43,25 +64,41 @@ function clickerino(n){
 	 "true" as the value of the variable, for it to be used in the last "else if".*/
 	 var falserino = juego.every(valor => valor.e==false);
  }
-  	if(juego[0].v== "x" && juego[1].v=="x" && juego[2].v=="x" ||
-		 juego[0].v== "x" && juego[3].v=="x" && juego[6].v=="x" ||
-	 	 juego[0].v== "x" && juego[4].v=="x" && juego[8].v=="x" ||
- 	 	 juego[2].v== "x" && juego[5].v=="x" && juego[8].v=="x" ||
-		 juego[2].v== "x" && juego[4].v=="x" && juego[6].v=="x" ||
-	 	 juego[1].v== "x" && juego[4].v=="x" && juego[7].v=="x" ||
-		 juego[6].v== "x" && juego[7].v=="x" && juego[8].v=="x" ||
-		 juego[3].v== "x" && juego[4].v=="x" && juego[5].v=="x"){
-		setTimeout(function(){ alert("Player 1 wins");resetear();}, 100);
-	}else if(juego[0].v== "o" && juego[1].v=="o" && juego[2].v=="o" ||
-					 juego[0].v== "o" && juego[3].v=="o" && juego[6].v=="o" ||
-				 	 juego[0].v== "o" && juego[4].v=="o" && juego[8].v=="o" ||
-			 	 	 juego[2].v== "o" && juego[5].v=="o" && juego[8].v=="o" ||
-					 juego[2].v== "o" && juego[4].v=="o" && juego[6].v=="o" ||
-				 	 juego[1].v== "o" && juego[4].v=="o" && juego[7].v=="o" ||
-					 juego[6].v== "o" && juego[7].v=="o" && juego[8].v=="o" ||
-					 juego[3].v== "o" && juego[4].v=="o" && juego[5].v=="o") {
-		setTimeout(function(){ alert("Player 2 wins");resetear();}, 100);
-	}else if (falserino){
-		setTimeout(function(){ alert("Draw");resetear();}, 100);
+ if(!done){
+	  	if(juego[0].v== "x" && juego[1].v=="x" && juego[2].v=="x" ||
+			 juego[0].v== "x" && juego[3].v=="x" && juego[6].v=="x" ||
+		 	 juego[0].v== "x" && juego[4].v=="x" && juego[8].v=="x" ||
+	 	 	 juego[2].v== "x" && juego[5].v=="x" && juego[8].v=="x" ||
+			 juego[2].v== "x" && juego[4].v=="x" && juego[6].v=="x" ||
+		 	 juego[1].v== "x" && juego[4].v=="x" && juego[7].v=="x" ||
+			 juego[6].v== "x" && juego[7].v=="x" && juego[8].v=="x" ||
+			 juego[3].v== "x" && juego[4].v=="x" && juego[5].v=="x"){
+				 tremble.innerHTML= xtext; done=true; tremble.className="animated tada"; t.style.backgroundColor="#e34a06";
+				 setTimeout(function(){resetear();}, 5000);
+		}else if(juego[0].v== "o" && juego[1].v=="o" && juego[2].v=="o" ||
+						 juego[0].v== "o" && juego[3].v=="o" && juego[6].v=="o" ||
+					 	 juego[0].v== "o" && juego[4].v=="o" && juego[8].v=="o" ||
+				 	 	 juego[2].v== "o" && juego[5].v=="o" && juego[8].v=="o" ||
+						 juego[2].v== "o" && juego[4].v=="o" && juego[6].v=="o" ||
+					 	 juego[1].v== "o" && juego[4].v=="o" && juego[7].v=="o" ||
+						 juego[6].v== "o" && juego[7].v=="o" && juego[8].v=="o" ||
+						 juego[3].v== "o" && juego[4].v=="o" && juego[5].v=="o") {
+					tremble.innerHTML= otext; done=true; tremble.className="animated rubberBand"; t.style.backgroundColor="#407026";
+					setTimeout(function(){resetear();}, 5000);
+		}else if (falserino){
+			tremble.innerHTML= dtext; done=true;
+			t.style.backgroundColor="#f9bd03";
+			tremble.className="animated flash";
+			setTimeout(function(){resetear();}, 5000);
+		}
 	}
 }
+
+var deftext = `Play!`;
+var xtext = `Player 1 wins!`;
+var otext = `Player 2 wins!`;
+var dtext = `Draw`;
+var descript = `
+	CSS grid, Javascript and <a href="https://cdnjs.com/libraries/animate.css/" target="_blank">animate.css</a><br>
+	Code on <a href="https://github.com/Flamerinus/3enraya" target="_blank">GitHub</a>.
+	`;
